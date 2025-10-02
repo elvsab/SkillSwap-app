786ac818ddedd553ac2a8250b84973668146904d
Vika Polshkova <vikagornushkina@yandex.ru>
2025-10-02 22:20:01 +0500

    Добавлен поиск по каталогу (#23)

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,41 @@
-import { useState } from 'react'
+import { useEffect, useState, useMemo } from 'react'
 //import reactLogo from './assets/react.svg'
 // импорт удалён — demo-asset удалён из проекта
 import viteLogo from '/vite.svg'
 import './App.css'
+import { SearchBar } from './searchBar/searchBar'
+import { useDebounce } from './shared/hooks/useDebounce'
+import { skillsData } from './searchBar/testData'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
+  // для поисковой строки
+  const [searchText, setSearchText] = useState('');
+  const debouncesSearchText = useDebounce(searchText, 300);
+
+  const filteredCards = useMemo(() => {
+    if (!debouncesSearchText.trim()) {
+      return skillsData
+    };
+    const lowercasedSearchText = debouncesSearchText.toLowerCase();
+
+    return skillsData.filter((card) => 
+      card.name.toLowerCase().includes(lowercasedSearchText) ||
+      card.description.toLowerCase().includes(lowercasedSearchText)
+    );
+  }, [debouncesSearchText]);
+
+
   return (
     <>
+      <div>
+        <SearchBar
+          searchText={searchText}
+          onSearchChange={setSearchText}
+        />
+      </div>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
---
+++ src/searchBar/testData.ts
@@ -0,0 +1,14 @@
+export const skillsData = [
+  {
+    id: '1',
+    name: 'Иван',
+    description: 'Игра на барабанах',
+    category: 'Может научить'
+  },
+  {
+    id: '2',
+    name: 'Анна',
+    description: 'Тайм менеджмент, Медитация',
+    category: 'Хочет научиться'
+  },
+];
