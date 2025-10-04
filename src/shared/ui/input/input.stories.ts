f1cdde48a058918430f7552c1fcede6a59563fd2
Kairat <baiborievk@yandex.ru>
2025-10-04 13:31:50 +0300

    Добавлен PasswordInput, исправлены наименования

--- src/shared/ui/input/input.stories.ts
+++ src/shared/ui/input/input.stories.ts
@@ -1,73 +1,77 @@
-import type { Meta, StoryObj } from '@storybook/react-vite';
+import type { Meta, StoryObj } from "@storybook/react-vite";
 
-import { Input } from './input';
+import { Input } from "./Input";
 
 const meta: Meta<typeof Input> = {
-    title: 'shared/ui/Input',
-    component: Input,
-    tags: ['autodocs'],
-    args: {
-        onChange: () => console.log('Input value has been changed')
-    }
+  title: "shared/ui/Input",
+  component: Input,
+  tags: ["autodocs"],
+  args: {
+    onChange: () => console.log("Input value has been changed"),
+  },
 } satisfies Meta<typeof Input>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
+export const InputPassword: Story = {
+  args: {
+    label: "Пароль",
+    type: "password",
+    name: "password",
+    placeholder: "Введите пароль",
+  },
+};
+
+export const InputPasswordFilled: Story = {
+  args: {
+    label: "Пароль",
+    type: "password",
+    name: "password",
+    placeholder: "Введите пароль",
+    value: "123test",
+  },
+};
+
+export const InputPasswordError: Story = {
+  args: {
+    label: "Пароль",
+    type: "password",
+    name: "password",
+    placeholder: "Введите пароль",
+    value: "123test",
+    error: true,
+    message: "Пароль должен содержать не менее 8 знаков",
+  },
+};
+
 export const InputEmail: Story = {
-    args: {
-        label: 'Email',
-        type: 'email',
-        name: 'email',
-        placeholder: 'Введите email',
-    }
+  args: {
+    label: "Email",
+    type: "email",
+    name: "email",
+    placeholder: "Введите email",
+  },
 };
 
 export const InputEmailFilled: Story = {
-    args: {
-        label: 'Email',
-        type: 'email',
-        name: 'email',
-        placeholder: 'Введите email',
-        value: 'test_email@test.ru'
-    }
+  args: {
+    label: "Email",
+    type: "email",
+    name: "email",
+    placeholder: "Введите email",
+    value: "test_email@test.ru",
+  },
 };
 
 export const InputEmailError: Story = {
-    args: {
-        label: 'Email',
-        type: 'email',
-        name: 'email',
-        placeholder: 'Введите email',
-        value: 'test_email@test.ru',
-        error: true,
-        message: 'Email уже используется'
-    }
-};
-
-export const InputName: Story = {
-    args: {
-        label: 'Имя',
-        type: 'text',
-        name: 'name',
-        placeholder: 'Введите имя'
-    }
-};
-
-export const InputWithExtraClass: Story = {
-    args: {
-        label: 'Имя',
-        type: 'text',
-        name: 'name',
-        placeholder: 'Введите имя',
-        className: 'input__name'
-    }
-};
-
-export const InputWithoutLabel: Story = {
-    args: {
-        type: 'text',
-        placeholder: 'Введите текст',
-        name: 'test'
-    }
+  args: {
+    label: "Email",
+    type: "email",
+    name: "email",
+    placeholder: "Введите email",
+    value: "test_email@test.ru",
+    error: true,
+    message: "Email уже используется",
+  },
 };
