e3e023a6bc95e4e9fd1e67cac6c6ccefeaa6fbe2
Elvira Latyshevskaia <elvsab@bk.ru>
2025-10-08 17:16:03 +0500

    added AuthProvider to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,14 @@ import { Provider } from 'react-redux'
 import './index.css'
 import App from './App.tsx'
 import store from './app/providers/store'
+import { AuthProvider } from './app/providers/authProvider'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </Provider>
   </StrictMode>,
 )
